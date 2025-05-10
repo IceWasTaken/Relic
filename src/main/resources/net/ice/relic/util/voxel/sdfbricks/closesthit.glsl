a42b5f1fa42513c39105b66a294f9e7ee1102fce
Ian <ian.hamilton.parsons@gmail.com>
2025-05-09 22:46:32 -0400

    initial

--- src/main/resources/net/ice/relic/util/voxel/sdfbricks/closesthit.glsl
+++
@@ -1,9 +0,0 @@
-#version 460
-#extension GL_EXT_ray_tracing : enable
-
-layout(location = 0) rayPayloadInEXT vec3 payload;
-hitAttributeEXT vec3 normal;
-
-void main(void) {
-    payload = normal;
-}
